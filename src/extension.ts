12dcd7480c734c74db89c2381fe5b025661c9e31
Daniel Mancebo <dama@bang-olufsen.dk>
2024-04-23 13:00:00 +0200

    Store path to faraday-tools

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,36 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { spawn } from 'child_process';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('runDH14', () => {
+		console.log("Running syslog")
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "faraday-syslog" is now active!');
+        const faradayToolsPath = vscode.workspace.getConfiguration().get('faradayToolsPath', 'python');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from faraday-syslog!');
-	});
+		const pythonProcess = spawn(faradayToolsPath + '/s.sh', []);
+        pythonProcess.stdout.on('data', (data) => {
+            // Process the data received from the Python tool
+            const output = data.toString();
+            // Display the output in VSCode
+            vscode.window.showInformationMessage(output);
+        });
 
-	context.subscriptions.push(disposable);
+        pythonProcess.stderr.on('data', (data) => {
+            // Handle any error output from the Python tool
+            console.error(`stderr: ${data}`);
+        });
+
+        pythonProcess.on('close', (code) => {
+            // Handle the close event
+            console.log(`child process exited with code ${code}`);
+        });
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
